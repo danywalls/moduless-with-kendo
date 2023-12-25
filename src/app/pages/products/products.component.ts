d88e368646cacab8cd284119a366d372f766d12d
Dany Paredes <danywalls4@gmail.com>
2023-12-25 11:03:18 +0100

    feat(products): add search functionality
    - Import FormsModule from @angular/forms
    - Add search input field with event binding to updateFilter() method
    - Add CSS styles for search container and input field
    - Update products loop to use $products() instead of products()
    - Update template to display search input and list of products dynamically
    
    The search functionality allows users to filter products based on their title.

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,9 +1,10 @@
-import {Component, inject} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {toSignal} from "@angular/core/rxjs-interop";
-import {CardModule} from "@progress/kendo-angular-layout";
-import {CurrencyPipe} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import { Component, computed, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CardModule } from '@progress/kendo-angular-layout';
+import { CurrencyPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 export type Product = {
   id: string;
@@ -13,22 +14,32 @@ export type Product = {
   image: string;
 };
 
-
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [
-    CardModule,
-    CurrencyPipe,
-    RouterLink
-  ],
+  imports: [CardModule, CurrencyPipe, RouterLink, FormsModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
-  http = inject(HttpClient);
-  products = toSignal(
-    this.http.get<Product[]>('https://fakestoreapi.com/products'),
-  );
+  #http = inject(HttpClient);
 
+  private $searchFilter = signal<string>('');
+
+  private $productsAPI = toSignal(
+    this.#http.get<Product[]>('https://fakestoreapi.com/products'),
+  );
+  $products = computed(() => {
+    return this.$productsAPI()?.filter((p) =>
+      p.title.toLowerCase().includes(this.$searchFilter()),
+    );
+  });
+
+  updateFilter(filter: string) {
+    if (filter.length > 3) {
+      this.$searchFilter.set(filter);
+    } else {
+      this.$searchFilter.set('');
+    }
+  }
 }
