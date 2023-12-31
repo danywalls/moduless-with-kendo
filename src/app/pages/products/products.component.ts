be8b99378aabe4000c5e631ffe34ae81cea4f7a5
Dany Paredes <danywalls4@gmail.com>
2023-12-31 18:02:29 +0100

    feat(products): add favorites functionality
    - Add `effect` import from `@angular/core`
    - Create `$favorites` signal to track favorite products
    - Add `addFavorite` function to add a product to favorites
    - Add `clearFavorites` function to clear all favorite products
    - Add `.favorites` class to styles
    - Update HTML to display favorite products and provide an option to clear them

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { CardModule } from '@progress/kendo-angular-layout';
@@ -22,18 +22,36 @@ export type Product = {
   styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
+  $favorites = signal<Product[]>([]);
   #http = inject(HttpClient);
-
   private $searchFilter = signal<string>('');
 
   private $productsAPI = toSignal(
     this.#http.get<Product[]>('https://fakestoreapi.com/products'),
   );
+
   $products = computed(() => {
     return this.$productsAPI()?.filter((p) =>
       p.title.toLowerCase().includes(this.$searchFilter()),
     );
   });
+
+  constructor() {
+    effect(() => {
+      if (this.$favorites().length > 3) {
+        console.log('reach limit');
+      }
+    });
+  }
+
+  addFavorite(product: Product) {
+    this.$favorites.update((p) => [...p, product]);
+    console.log(this.$favorites());
+  }
+
+  clearFavorites(): void {
+    this.$favorites.set([]);
+  }
 
   updateFilter(filter: string) {
     if (filter.length > 3) {
