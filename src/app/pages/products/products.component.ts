59bda0b8809019e9be30f7bb63ee05484b80541d
Dany Paredes <danywalls4@gmail.com>
2024-01-03 18:29:33 +0100

    add fileter
    feat: Add filter functionality and update styles
    
    - Added a search input field to filter products by name
    - Updated the layout of the favorites section
    - Changed the button in the favorites section from an h2 element to a button element
    - Updated the section id for products from "products" to "#products"
    - Adjusted the styling for the products container, including flex-direction, padding, and gap
    - Added media query for responsive design at max-width 768px
    - Added a new CSS class ".favorites" with styling for images inside it
    - Removed unnecessary imports and declarations in the ProductsComponent TypeScript file
    - Updated logic in ProductsComponent constructor to add a CSS class when there are more than or equal to 3 favorite items

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -3,9 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { CardModule } from '@progress/kendo-angular-layout';
 import { CurrencyPipe } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { debounceTime } from 'rxjs';
 
 export type Product = {
   id: string;
@@ -18,21 +15,20 @@ export type Product = {
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CardModule, CurrencyPipe, RouterLink, FormsModule],
+  imports: [CardModule, CurrencyPipe],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
   $favorites = signal<Product[]>([]);
-  favoriteMessage = 'You can add 3 favorite items 😊';
+  http = inject(HttpClient);
+
   #http = inject(HttpClient);
   private $searchFilter = signal<string>('');
   private $productsAPI = toSignal(
-    this.#http
-      .get<Product[]>('https://fakestoreapi.com/products')
-      .pipe(debounceTime(3000)),
+    this.#http.get<Product[]>('https://fakestoreapi.com/products'),
   );
-  $products = computed(() => {
+  products = computed(() => {
     return this.$productsAPI()?.filter((p) =>
       p.title.toLowerCase().includes(this.$searchFilter()),
     );
@@ -40,16 +36,14 @@ export class ProductsComponent {
 
   constructor() {
     effect(() => {
-      if (this.$favorites().length === 3) {
-        this.favoriteMessage =
-          'You reach the limit, please go to purchase area ✔';
+      if (this.$favorites().length >= 3) {
+        document.body.classList.add('red');
       }
     });
   }
 
   addFavorite(product: Product) {
     this.$favorites.update((p) => [...p, product]);
-    console.log(this.$favorites());
   }
 
   clearFavorites(): void {
